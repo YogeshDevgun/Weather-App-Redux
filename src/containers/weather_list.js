78d8978342e8433f07a84cb26beabf5413a6b947
Yogesh Devgun <yogeshdevgun@gmail.com>
2017-09-12 20:45:56 +0530

    Google Map integrated with some styling! Enjoy :)

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
+
 class WeatherList extends Component {
   constructor(props){
     super(props);
@@ -11,9 +13,12 @@ class WeatherList extends Component {
     const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp-273);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
-
+    // const lon = cityData.city.coord.lon;
+    // const lat = cityData.city.coord.lat;
+    const { lon, lat } = cityData.city.coord;
     return(
       <tr key={name}>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td>{name}</td>
         <td>
           <Chart data={temps} color="orange" units="deg C"/>
@@ -33,6 +38,7 @@ class WeatherList extends Component {
       <table className="table table-hover">
         <thead>
           <tr>
+            <th>Map</th>
             <th>City</th>
             <th>Temperature (deg C)</th>
             <th>Pressure (hPa)</th>
