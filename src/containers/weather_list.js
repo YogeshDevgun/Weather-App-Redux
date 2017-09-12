e199a7ed90ca0b39d1bf803c81b9abdbeed6d963
Yogesh Devgun <yogeshdevgun@gmail.com>
2017-09-12 19:43:52 +0530

    Giving labels to maps!

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -8,7 +8,7 @@ class WeatherList extends Component {
 
   renderWeather(cityData){
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp-273);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
 
@@ -16,13 +16,13 @@ class WeatherList extends Component {
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Chart data={temps} color="orange" />
+          <Chart data={temps} color="orange" units="deg C"/>
         </td>
         <td>
-          <Chart data={pressures} color="green" />
+          <Chart data={pressures} color="green" units="hPa" />
         </td>
         <td>
-          <Chart data={humidities} color="blue" />
+          <Chart data={humidities} color="blue" units="%"/>
         </td>
       </tr>
     );
@@ -34,9 +34,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (deg C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
